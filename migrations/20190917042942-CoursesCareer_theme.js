7db671b20cbe3df6725611d9d808ffac5b4f83fd
Gaman <gamanm@yandex.ru>
2019-09-17 11:39:38 +0700

    add correct files, change migrations name

---
+++ migrations/20190917042942-CoursesCareer_theme.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('coursesCareer_theme', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      careerPathId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'careerPath',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      themeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'themes',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('coursesCareer_theme');
+  }
+};
